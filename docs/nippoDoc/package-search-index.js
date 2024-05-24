d3e68f97c6a6e6c5a777c74681bde08418edbbb8
yas-ribe <128051916+yas-ribe@users.noreply.github.com>
2024-05-24 12:48:35 -0300

    nippo

---
+++ docs/nippoDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.nippodents"},{"l":"com.example.nippodents.activity"},{"l":"com.example.nippodents.interfaces"},{"l":"com.example.nippodents.models"},{"l":"com.example.nippodents.services"},{"l":"com.example.nippodents.utils"}];updateSearchResults();
